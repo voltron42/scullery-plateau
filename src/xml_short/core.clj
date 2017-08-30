f17ad933fc39c69979a9be4acac771f670184567
Daniel Johnson <daniel.johnson@surescripts.com>
2017-08-30 16:39:00 -0500

    adding html entity encoding to shorthand

--- src/xml_short/core.clj
+++ src/xml_short/core.clj
@@ -1,7 +1,15 @@
-(ns xml-short.core)
+(ns xml-short.core
+  (:require [clojure.string :as s]))
 
 (defn- name-to-keyword [tag]
   (keyword (str (if (nil? (namespace tag)) "" (str (namespace tag) ":")) (name tag))))
+
+(defn- encode-html-entities [s]
+  (s/escape s {\< "&lt;"
+               \> "&gt;"
+               \& "&amp;"
+               \' "&apos;"
+               \" "&quot;"}))
 
 (defn x-pand [body]
   (let [bodynext (if (symbol? body) [body] body)
@@ -11,9 +19,10 @@
         [content attrs] (if (map? attrs)
                           [content (reduce (fn [out [k v]] (assoc out (name-to-keyword k) v)) {} attrs)]
                           [(into [attrs] content) {}])
+        attrs (reduce (fn [out [k v]] (assoc out k (encode-html-entities (str v)))) {} attrs)
         content (if (or (nil? content) (empty? content))
                   content
-                  (mapv #(if (or (vector? %) (symbol? %)) (x-pand %) %) content))]
+                  (mapv #(if (or (vector? %) (symbol? %)) (x-pand %) (encode-html-entities (str %))) content))]
     (reduce (fn [out [k v]] (if (or (empty? v) (nil? v)) out (assoc out k v)))
             {:tag tag}
             {:attrs attrs :content content})))
