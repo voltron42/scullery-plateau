773c4876e73f37ec3da26d155c81d25840fe5e70
Daniel Johnson <daniel.johnson@surescripts.com>
2017-08-31 16:39:50 -0500

    adding functionality to template core

--- src/template/core.clj
+++ src/template/core.clj
@@ -1,5 +1,7 @@
 (ns template.core)
 
+(defn- default-ops {'entry-set (fn [my-map] (mapv (fn [[k v]] {:k k :v v}) (sorted-map my-map)))})
+
 (defn- resolve-tpl [op-map data out-data tpl]
   (cond
     (map? tpl) (reduce (fn [out [k v]] (assoc out k (resolve-tpl op-map data [] v))) {} tpl)
@@ -15,7 +17,8 @@
     (list? tpl) (let [[op & args] tpl]
                   (cond
                     (map? op) (let [[label col] (first op)
-                                    col (data col)
+                                    col (if (keyword? col) (data col) col)
+                                    col (if (list? col) (resolve-tpl op-map data [] col) col)
                                     results (reduce (fn [out elem]
                                                       (let [new-data (if (map? elem)
                                                                        (reduce (fn [obj [k v]]
@@ -39,4 +42,4 @@
 (defn build-template-factory [op-map]
   (fn build-template [tpl]
     (fn resolve-template [data]
-      (resolve-tpl op-map data [] tpl))))
+      (resolve-tpl (merge op-map default-ops) data [] tpl))))
