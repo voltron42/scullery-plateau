44981cbad3fe3fd0bbde9e83262caf0d97915f8f
Daniel Johnson <daniel.johnson@surescripts.com>
2017-09-01 16:55:00 -0500

    adding functionality to template

--- src/template/core.clj
+++ src/template/core.clj
@@ -1,6 +1,21 @@
 (ns template.core)
 
-(defn- default-ops {'entry-set (fn [my-map] (mapv (fn [[k v]] {:k k :v v}) (sorted-map my-map)))})
+(def ^:private default-ops {'entry-set (fn [my-map] (mapv (fn [[k v]] {:k k :v v}) (sorted-map my-map)))})
+
+(defn- resolve-col [col op-map data recurse]
+  (let [recursive-call #(resolve-col % op-map data recurse)]
+    (cond
+      (keyword? col) (data col)
+      (list? col) (recurse op-map data [] col)
+      (vector? col) (mapv recursive-call col)
+      (map? col) (reduce (fn [out [k v]] (assoc out k (recursive-call v))) {} col)
+      :else col)))
+
+(defn- label-data [label data]
+  (cond
+    (vector? data) (mapv (partial label-data label) data)
+    (map? data) (reduce (fn [out [k v]] (assoc out (keyword (name label) (name k)) v)) {} data)
+    :else (assoc {} label data)))
 
 (defn- resolve-tpl [op-map data out-data tpl]
   (cond
@@ -17,17 +32,13 @@
     (list? tpl) (let [[op & args] tpl]
                   (cond
                     (map? op) (let [[label col] (first op)
-                                    col (if (keyword? col) (data col) col)
-                                    col (if (list? col) (resolve-tpl op-map data [] col) col)
-                                    results (reduce (fn [out elem]
-                                                      (let [new-data (if (map? elem)
-                                                                       (reduce (fn [obj [k v]]
-                                                                                 (assoc obj (keyword (name label) (name k)) v))
-                                                                               data elem)
-                                                                       (assoc data label elem))]
-                                                        (reduce #(into %1 [(resolve-tpl op-map new-data [] %2)]) out args)))
-                                                    out-data col)]
-                                results)
+                                    col (resolve-col col op-map data resolve-tpl)
+                                    new-data (label-data label col)
+                                    append (fn [out item] (reduce #(into %1 [(resolve-tpl op-map (merge data item) [] %2)]) out args))
+                                    output (if (vector? new-data)
+                                             (reduce append out-data new-data)
+                                             (append out-data new-data))]
+                                output)
                     (list? op) (when (resolve-tpl op-map data [] op)
                                  (reduce (partial resolve-tpl op-map data) out-data args))
                     (symbol? op) (let [func (let [temp-func (resolve op)] (if-not (nil? temp-func)
@@ -39,7 +50,24 @@
     (keyword? tpl) (tpl data)
     :else tpl))
 
+(defn flatten-output
+  ([input] (flatten-output (empty input) input))
+  ([output input]
+   (let [init (empty input)
+         wrap #(conj init %)]
+     (vec
+       (reduce (fn [out in]
+                 (if (coll? in)
+                   (if (every? coll? in)
+                     (if (= 1 (count in))
+                       (concat out (flatten-output in))
+                       (reduce #(concat %1 (wrap (flatten-output %2))) out in))
+                     (concat out (wrap (map #(if (coll? %) (flatten-output %) %) in))))
+                   (concat out (wrap in))))
+               output input)))))
+
 (defn build-template-factory [op-map]
   (fn build-template [tpl]
     (fn resolve-template [data]
-      (resolve-tpl (merge op-map default-ops) data [] tpl))))
+      (let [resolved (resolve-tpl (merge op-map default-ops) data [] tpl)]
+        (flatten-output resolved)))))
---
+++ test/template/core_test.clj
@@ -0,0 +1,135 @@
+(ns template.core-test
+  (:require [clojure.test :refer :all]
+            [template.core :refer :all]))
+
+(deftest test-example
+  (let [example-tpl '[html {lang "en"}
+                      [head
+                       [meta {charset "utf-8"}]
+                       [title :title]
+                       [meta {name "viewport" content "width=device-width, initial-scale=1.0"}]
+                       [meta {name "description" content :description}]
+                       [meta {name "author" content :author}]]
+                      [body
+                       ({:element :sections}
+                         [div {class "section"}
+                          [h1 :element/title]
+                          ((some? :element/description)
+                            [p :element/description])])]]
+        example-data {:title "This is the title"
+                      :description "This is the description"
+                      :author "I am the author"
+                      :sections [{:title "Section One"}
+                                 {:title "Section Two"
+                                  :description "This is the description for section two"}]}
+        expected-result '[html {lang "en"}
+                          [head
+                           [meta {charset "utf-8"}]
+                           [title "This is the title"]
+                           [meta {name "viewport" content "width=device-width, initial-scale=1.0"}]
+                           [meta {name "description" content "This is the description"}]
+                           [meta {name "author" content "I am the author"}]]
+                          [body
+                           [div {class "section"}
+                            [h1 "Section One"]]
+                           [div {class "section"}
+                            [h1 "Section Two"]
+                            [p "This is the description for section two"]]]]]
+        (is (= expected-result (((build-template-factory {}) example-tpl) example-data)))))
+
+(deftest test-example-2
+  (let [ex-tpl '[ul
+                 ({:x (vec (range 3))}
+                   ({:y (vec (range 5))}
+                     [li (str "(x,y) = (" :x "," :y ")")]))]
+        ex-data {}
+        ex-result '[ul
+                    [li "(x,y) = (0,0)"]
+                    [li "(x,y) = (0,1)"]
+                    [li "(x,y) = (0,2)"]
+                    [li "(x,y) = (0,3)"]
+                    [li "(x,y) = (0,4)"]
+                    [li "(x,y) = (1,0)"]
+                    [li "(x,y) = (1,1)"]
+                    [li "(x,y) = (1,2)"]
+                    [li "(x,y) = (1,3)"]
+                    [li "(x,y) = (1,4)"]
+                    [li "(x,y) = (2,0)"]
+                    [li "(x,y) = (2,1)"]
+                    [li "(x,y) = (2,2)"]
+                    [li "(x,y) = (2,3)"]
+                    [li "(x,y) = (2,4)"]]]
+    (is (= ex-result (((build-template-factory {}) ex-tpl) ex-data)))))
+
+(deftest test-flatten-output-1
+  (is (= (flatten-output '[ul
+                          [[li
+                            "(x,y) = (0,0)"]
+                           [li
+                            "(x,y) = (0,1)"]
+                           [li
+                            "(x,y) = (0,2)"]
+                           [li
+                            "(x,y) = (0,3)"]
+                           [li
+                            "(x,y) = (0,4)"]]
+                          [[li
+                            "(x,y) = (1,0)"]
+                           [li
+                            "(x,y) = (1,1)"]
+                           [li
+                            "(x,y) = (1,2)"]
+                           [li
+                            "(x,y) = (1,3)"]
+                           [li
+                            "(x,y) = (1,4)"]]
+                          [[li
+                            "(x,y) = (2,0)"]
+                           [[li
+                             "(x,y) = (2,1)"]
+                            [li
+                             "(x,y) = (2,2)"]]
+                           [li
+                            "(x,y) = (2,3)"]
+                           [li
+                            "(x,y) = (2,4)"]]])
+         '[ul
+          [li
+           "(x,y) = (0,0)"]
+          [li
+           "(x,y) = (0,1)"]
+          [li
+           "(x,y) = (0,2)"]
+          [li
+           "(x,y) = (0,3)"]
+          [li
+           "(x,y) = (0,4)"]
+          [li
+           "(x,y) = (1,0)"]
+          [li
+           "(x,y) = (1,1)"]
+          [li
+           "(x,y) = (1,2)"]
+          [li
+           "(x,y) = (1,3)"]
+          [li
+           "(x,y) = (1,4)"]
+          [li
+           "(x,y) = (2,0)"]
+          [li
+           "(x,y) = (2,1)"]
+          [li
+           "(x,y) = (2,2)"]
+          [li
+           "(x,y) = (2,3)"]
+          [li
+           "(x,y) = (2,4)"]])))
+
+(deftest test-flatten-output-2
+  (is (= (flatten-output [["Do"] ["Nothing"]]) [["Do"] ["Nothing"]]))
+
+
+  (is (= (flatten-output [[[[:a :b]]] [[:c :d]] [:e :f]]) [[:a :b] [:c :d] [:e :f]]))
+
+
+  (is (= (flatten-output '((1 2)((3 4)((((5 6))))))) '((1 2)(3 4)(5 6)))))
