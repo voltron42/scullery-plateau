a13bc531dcc93fb4e1a0d076bfcbdc5b7de115dd
Daniel Johnson <dajohnson1978@gmail.com>
2017-09-05 22:06:30 -0500

    fleshing out more bars of zelda, and scales

--- src/music/audio.clj
+++ src/music/audio.clj
@@ -103,10 +103,12 @@
 (defn tone-freq [x]
   (-> (Math/pow 2 (/ x 11)) (* 440) (/ 512)))
 
-(def ^:private notes {:Ab -1 :A 0 :A# 1 :Bb 1 :B 2
+(def ^:private notes {
+                      :Ab -1 :A 0 :A# 1 :Bb 1 :B 2
                        :C 3 :C# 4 :Db 4 :D 5 :D# 6
                        :Eb 6 :E 7 :F 8 :F# 9
-                       :Gb 9 :G 10 :G# 11})
+                       :Gb 9 :G 10 :G# 11
+                      })
 
 (defn- parse-int [s] (Integer/parseInt s))
 
@@ -144,4 +146,8 @@
   (play-song (edn/read-string (slurp "resources/sheet/mountainking.edn")) 33 2000))
 
 (defn start-playing-zelda []
-  (play-song (edn/read-string (slurp "resources/sheet/zelda.edn")) 33 2000))
+  (play-song (edn/read-string (slurp "resources/sheet/zelda.edn")) 32 2000))
+
+(defn start-playing-scales []
+  (play-song (edn/read-string (slurp "resources/sheet/scales.edn")) 32 2000))
+
