f115c97c5eed645f88ec98304c274c9c3b248a92
Daniel Johnson <daniel.johnson@surescripts.com>
2017-10-13 13:53:59 -0500

    more fight club 5 modelling

--- src/fight_club_5/model.clj
+++ src/fight_club_5/model.clj
@@ -1,5 +1,12 @@
 (ns fight-club-5.model
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s])
+  (:import (javafx.scene.layout Background)
+           (org.apache.batik.svggen.font.table Feature)
+           (java.lang.reflect Modifier)))
+
+(s/defschema Ability (s/enum :Strength :Dexterity :Constitution :Wisdom :Intelligence :Charisma))
+
+(s/defschema Skill (s/enum :Insight :Religion :Deception :Sleight-of-Hand ))
 
 (s/defschema Casting-Time s/Str)
 
@@ -25,9 +32,53 @@
    :range Game-Range
    :components #{Spell-Component}
    :duration Effect-Duration
-   :classes [Class-Name]
+   :classes #{Class-Name}
    :text [Spell-Description]
    :roll [Roll]})
 
+(s/defschema Trait-Name s/Str)
+
+(s/defschema Trait-Text s/Str)
+
+(s/defschema Background
+  {:name s/Str
+   :proficiency #{Skill}
+   :traits {Trait-Name [Trait-Text]}})
+
+(s/defschema Modifier
+  {:category s/Str
+   :description s/Str})
+
+(s/defschema Feat
+  {:name        s/Str
+   :description [s/Str]
+   :modifier    Modifier
+   :prerequisite s/Str})
+
+(s/defschema Feature
+  {:name s/Str
+   :description [s/Str]
+   :optional s/Bool})
+
+(s/defschema Autolevel
+  {:level s/Int
+   :features [Feature]
+   :slots [s/Int]})
+
+(s/defschema Class
+  {:name s/Str
+   :hd s/Int
+   :proficiency #{(s/either Skill Ability)}
+   (s/optional-key :spell-ability) Ability
+   :autolevel Autolevel})
+
+(s/defschema Race
+  {})
+
 (s/defschema Compendium
-  {:spells [Spell]})
+  {:spells [Spell]
+   :backgrounds [Background]
+   :feats [Feat]
+   :classes [Class]
+   :races [Race]
+   })
