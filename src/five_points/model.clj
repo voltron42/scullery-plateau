3932f4ef190b24ba3779a7a28b62f14b7bfbdd93
Daniel Johnson <dajohnson1978@gmail.com>
2017-11-18 15:51:39 -0600

    more work done fleshing out schema

--- src/five_points/model.clj
+++ src/five_points/model.clj
@@ -1,18 +1,36 @@
 (ns five-points.model
   (:require [schema.core :as s]
-            [five-points.validate :as v]))
+            [five-points.validate :as v])
+  (:import (javafx.scene.effect Effect)
+           (cljpdf.text Chapter)))
 
-(s/defschema Kin-Id (s/constrained s/Str #(re-matches #"kin[A-Za-z0-9]{10}" %) 'Kin-Id))
+(s/defschema Kin-Id (s/constrained s/Str #(re-matches #"kin_[A-Za-z0-9]{10}" %) 'Kin-Id))
 
-(s/defschema Caste-Id (s/constrained s/Str #(re-matches #"caste[A-Za-z0-9]{10}" %) 'Caste-Id))
+(s/defschema Caste-Id (s/constrained s/Str #(re-matches #"caste_[A-Za-z0-9]{10}" %) 'Caste-Id))
 
-(s/defschema Skill-Id (s/constrained s/Str #(re-matches #"skill[A-Za-z0-9]{10}" %) 'Skill-Id))
+(s/defschema Skill-Id (s/constrained s/Str #(re-matches #"skill_[A-Za-z0-9]{10}" %) 'Skill-Id))
 
-(s/defschema Skill-Set-Id (s/constrained s/Str #(re-matches #"skst[A-Za-z0-9]{10}" %) 'Skill-Set-Id))
+(s/defschema Skill-Set-Id (s/constrained s/Str #(re-matches #"skst_[A-Za-z0-9]{10}" %) 'Skill-Set-Id))
 
-(s/defschema Item-Id (s/constrained s/Str #(re-matches #"item[A-Za-z0-9]{10}" %) 'Item-Id))
+(s/defschema Item-Id (s/constrained s/Str #(re-matches #"item_[A-Za-z0-9]{10}" %) 'Item-Id))
 
-(s/defschema Char-Id (s/constrained s/Str #(re-matches #"char[A-Za-z0-9]{10}" %) 'Char-Id))
+(s/defschema Char-Id (s/constrained s/Str #(re-matches #"char_[A-Za-z0-9]{10}" %) 'Char-Id))
+
+(s/defschema Check-Id (s/constrained s/Str #(re-matches #"chk_[A-Za-z0-9]{10}" %) 'Check-Id))
+
+(s/defschema Damage-Id (s/constrained s/Str #(re-matches #"dmg_[A-Za-z0-9]{10}" %) 'Damage-Id))
+
+(s/defschema Condition-Id (s/constrained s/Str #(re-matches #"cond_[A-Za-z0-9]{10}" %) 'Condition-Id))
+
+(s/defschema Obstacle-Id (s/constrained s/Str #(re-matches #"obst_[A-Za-z0-9]{10}" %) 'Obstacle-Id))
+
+(s/defschema Maze-Id (s/constrained s/Str #(re-matches #"maze_[A-Za-z0-9]{10}" %) 'Maze-Id))
+
+(s/defschema Room-Id (s/constrained s/Str #(re-matches #"room_[A-Za-z0-9]{10}" %) 'Room-Id))
+
+(s/defschema NPC-Id (s/constrained s/Str #(re-matches #"npc_[A-Za-z0-9]{10}" %) 'NPC-Id))
+
+(s/defschema Question-Id (s/constrained s/Str #(re-matches #"q_[A-Za-z0-9]{10}" %) 'Question-Id))
 
 (s/defschema GameTime {:value s/Int
                        :unit (s/enum :Minor-Action :Major-Action :Minute :Hour :Day :Month :Year)})
@@ -28,15 +46,47 @@
           :Sense
           :Charm))
 
+(s/defschema Custom-Check
+  {:id Check-Id
+   :name s/Str
+   :description s/Str
+   :stat Stat})
+
 (s/defschema Check (s/enum
                      :Push :Pull :Jump :Catch :Throw :Run :Climb :Grab :Hold :Block :Melee-Attack
                      :Balance :Tumble :Dodge :Parry :Range-Attack :Hide
                      :Resist :Heal-Self
-                     :Find :Recall :Learn :Triage :Cure
+                     :Find :Recall :Learn :Triage :Cure :Aim
                      :Look :Smell :Taste :Touch :Hear :Detect-Psychic
                      :Inspire :Sway :Mock :Tame :Lie))
 
 (s/defschema Stats {Stat Score})
+
+(s/defschema Impact (s/enum :immune :guarded :exposed))
+
+(s/defschema Size (s/enum :tiny :small :medium :large :giant :huge :gargantuan))
+
+(s/defschema Damage-Type
+  {:id Damage-Id
+   :name s/Str
+   :description s/Str})
+
+(s/defschema Susceptability
+  {:types #{Damage-Id}
+   :impact Impact})
+
+(s/defschema Limitation
+  (s/cond-pre
+    Condition-Id
+    Stats
+    {(s/enum :edge :burden) (s/cond-pre Check Stat Check-Id)}
+    Susceptability))
+
+(s/defschema Condition
+  {:id Condition-Id
+   :name s/Str
+   :description s/Str
+   :limitations #{Limitation}})
 
 (s/defschema Prerequisite
   (s/constrained
@@ -46,13 +96,25 @@
      (s/optional-key :skills) #{Skill-Id}}
     not-empty))
 
-(s/defschema Bonus
+(s/defschema All-Checks (s/cond-pre Check Stat Check-Id))
+
+(s/defschema Effect-Check-Value
+  {:score (s/cond-pre Score (s/enum :talent :mastery))
+   (s/optional-key :multiplier) (s/cond-pre s/Int :level)})
+
+(s/defschema Effect
   {:description s/Str
-   :check (s/either Check Stat)
-   :value (s/either Score :talent {:talent s/Int} {:level Score} {:talent :level})
    (s/optional-key :lvl) s/Int
    (s/optional-key :invocation) GameTime
-   (s/optional-key :duration) GameTime})
+   (s/optional-key :duration) GameTime
+   (s/optional-key :frequency) GameTime
+   :target {(s/enum :count :lvl-) s/Int
+            :values #{(s/enum :self :ally :opponent)}}
+   :effect (s/cond-pre
+             {Condition-Id {:check All-Checks
+                            :mark s/Int}}
+             {(s/enum :edge :burden) All-Checks}
+             {(s/cond-pre All-Checks Damage-Id) Effect-Check-Value})})
 
 (s/defschema Starting
   (s/constrained
@@ -66,6 +128,7 @@
    :name s/Str
    :stats Stats
    :description s/Str
+   (s/optional-key :size) Size
    (s/optional-key :starting) Starting})
 
 (s/defschema Caste
@@ -82,7 +145,7 @@
    :type [s/Str]
    (s/optional-key :lvl) s/Int
    (s/optional-key :prerequisites) Prerequisite
-   (s/optional-key :bonuses) #{Bonus}})
+   (s/optional-key :effects) #{Effect}})
 
 (s/defschema Skill-Set
   {:id Skill-Set-Id
@@ -99,7 +162,7 @@
    (s/optional-key :lvl) s/Int
    (s/optional-key :bonded) (s/eq true)
    (s/optional-key :prerequisites) Prerequisite
-   (s/optional-key :bonuses) #{Bonus}
+   (s/optional-key :effects) #{Effect}
    (s/optional-key :heft) (s/enum :heavy :light)
    (s/optional-key :time-to-equip) GameTime
    :cost {:value s/Int :unit s/Str}})
@@ -114,13 +177,87 @@
    :skills #{Skill-Id}
    :items #{Item-Id}})
 
+(s/defschema Solution
+  {:check All-Checks
+   :mark s/Int
+   (s/enum :succeed :fail) Effect})
+
+(s/defschema Obstacle
+  {:id Obstacle-Id
+   :name s/Str
+   :description s/Str
+   :effects #{Effect}
+   :solutions #{Solution}})
+
 (s/defschema Context
   (s/constrained
-    {:kin {Kin-Id Kin}
-     :castes {Caste-Id Caste-Id}
+    {:checks {Check-Id Custom-Check}
+     :damage-types {Damage-Id Damage-Type}
+     :conditions {Condition-Id Condition}
+     :kin {Kin-Id Kin}
+     :castes {Caste-Id Caste}
      :items {Item-Id Item}
      :skills {Skill-Id Skill}
      :skill-sets {Skill-Set-Id Skill-Set}
-     :characters {Char-Id Character}}
+     :characters {Char-Id Character}
+     :obstacles {Obstacle-Id Obstacle}}
     v/validate-compendium
     'Compendium))
+
+(s/defschema Answer
+  {:text s/Str
+   :next #{Question-Id}})
+
+(s/defschema Question-Node
+{:id Question-Id
+ :question s/Str
+ :answer (s/cond-pre
+           Answer
+           {:checks #{{:check All-Checks
+                       :mark s/Int}}
+            :fail Answer
+            :succeed Answer})})
+
+(s/defschema NPC
+  {:character Char-Id
+   :questions {Question-Id Question-Node}
+   :base-line #{Question-Id}})
+
+(s/defschema Position {:x s/Int :y s/Int})
+
+(s/defschema Door
+  {:label s/Str
+   :description s/Str
+   :to Room-Id})
+
+(s/defschema Room
+  {:id Room-Id
+   :name s/Str
+   :description s/Str
+   :shape (s/cond-pre
+            {:width s/Int :height s/Int}
+            {:points [Position]})
+   (s/optional-key :doors) #{Door}
+   (s/optional-key :contents){Position (s/cond-pre Char-Id NPC-Id Obstacle-Id Item-Id)}})
+
+(s/defschema Maze
+  {:rooms {Room-Id Room}
+   :entrance Room-Id
+   :exit Room-Id})
+
+(s/defschema Chapter
+  {:intro s/Str
+   :outro s/Str
+   :maze Maze-Id})
+
+(s/defschema Story
+  {:title s/Str
+   :prologue s/Str
+   :epilogue s/Str
+   :chapters [Chapter]})
+
+(s/defschema Adventure
+  {:context Context
+   :story Story
+   :mazes {Maze-Id Maze}
+   :NPCs {NPC-Id NPC}})
